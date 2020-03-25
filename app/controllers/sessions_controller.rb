068abb3d8e528a2225488d2028df01152e5cc865
k.kei <acidjannedoor@yahoo.co.jp>
2020-03-25 06:10:52 +0000

    session create ガード節に修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,18 +5,17 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      if current_user.admin?
-        redirect_to users_url
-      else
-        redirect_back_or user
-        return
-      end
-      flash.now[:danger] = '認証に失敗しました。'
-      render :new
+    return unless user&.authenticate(params[:session][:password])
+    log_in user
+    params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+    if current_user.admin?
+      redirect_to users_url
+    else
+      redirect_back_or user
+      return
     end
+    flash.now[:danger] = '認証に失敗しました。'
+    render :new
   end
 
   def destroy
