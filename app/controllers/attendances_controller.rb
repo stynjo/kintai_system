27776345a2e64a0b5671ea30a4e93ceeb72ea383
k.kei <acidjannedoor@yahoo.co.jp>
2020-03-26 02:25:04 +0000

    トランザクションブロックが長いのを修正 off 33

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -41,13 +41,11 @@ class AttendancesController < ApplicationController
             success = false
             break
           end
-        elsif item['edit_started_at'].present? && item['edit_finished_at'].present? && item['change_attendance_id'].present? && item['edit_started_at'] > item['edit_finished_at']
+        elsif item['edit_started_at'].present? && item['edit_finished_at'].present? && item['change_attendance_id'].present? && item['edit_started_at'] > item['edit_finished_at'] && item['edit_one_month_tomorrow'] == 'true'
           attendance = Attendance.find(id)
+          attendance.edit_finished_at = attendance.edit_finished_at.tomorrow
           attendance.update_attributes!(item)
-          if attendance.edit_one_month_tomorrow == true
-            attendance.edit_finished_at = attendance.edit_finished_at.tomorrow
-            attendance.save
-          elsif attendance.edit_one_month_tomorrow == false
+          if attendance.edit_one_month_tomorrow == false
             success = false
             break
           end
